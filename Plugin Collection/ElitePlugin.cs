688110718773a2b539cae469cbb0df9847e33fe4
Xèng <quangxengvn@gmail.com>
2019-04-22 17:41:27 +0700

    Update ElitePlugin.cs

--- Plugin Collection/ElitePlugin.cs
+++ Plugin Collection/ElitePlugin.cs
@@ -4,10 +4,8 @@ using System.Linq;
 
 namespace Turbo.Plugins.Default
 {
-
     public class ElitePlugin : BasePlugin, IInGameWorldPainter
     {
-
         /************************************************************************/
         /* EliteCirclePlugin                                                    */
         /************************************************************************/
@@ -20,13 +18,15 @@ namespace Turbo.Plugins.Default
         //public WorldDecoratorCollection RareMinionDecorator { get; set; }   // Elite Minion
         public WorldDecoratorCollection UniqueDecorator { get; set; }   //Purple
         public WorldDecoratorCollection BossDecorator { get; set; }   //Boss
-        public TopLabelDecorator TagDecorator { get; set; }
+        public TopLabelDecorator EliteHealthDecorator { get; set; }
+        public int EliteHealthBlockSize;
         public void LoadEliteCirclePlugin()
         {
-            TagDecorator = new TopLabelDecorator(Hud)
+            EliteHealthDecorator = new TopLabelDecorator(Hud)
             {
-                TextFont = Hud.Render.CreateFont("tahoma", 8, 200, 255, 255, 255, true, false, false),
+                TextFont = Hud.Render.CreateFont("tahoma", 8, 200, 255, 36, 0, true, false, false),
             };
+            EliteHealthBlockSize = (int)(Hud.Window.Size.Width / 50);
 
             var shadowBrush = Hud.Render.CreateBrush(96, 0, 0, 0, 1);
 
@@ -136,7 +136,6 @@ namespace Turbo.Plugins.Default
             //	        ShapePainter = new CircleShapePainter(Hud)
             //	    }
             //	);
-
             UniqueDecorator = new WorldDecoratorCollection(
                 new GroundCircleDecorator(Hud)
                 {
@@ -205,12 +204,10 @@ namespace Turbo.Plugins.Default
                 {
                     monstersElite.Add(monster);
                 }
-
                 //	if (monster.Rarity == ActorRarity.RareMinion)
                 //	{
                 //	    RareMinionDecorator.Paint(layer, monster, monster.FloorCoordinate, monster.SnoMonster.NameLocalized);
                 //	}
-
                 if (monster.Rarity == ActorRarity.Unique)
                 {
                     UniqueDecorator.Paint(layer, monster, monster.FloorCoordinate, monster.SnoMonster.NameLocalized);
@@ -223,11 +220,11 @@ namespace Turbo.Plugins.Default
             }
             foreach (var monster in monstersElite)
             {
-                var currentMonsterHealthPercent = monster.CurHealth * 100.0 / monster.MaxHealth;
-
                 if (monster.SummonerAcdDynamicId == 0)
                 {
-                    bool flag = true;
+                    var currentMonsterHealthPercent = monster.CurHealth * 100.0 / monster.MaxHealth;
+
+                    bool flagIsNotJuggernaut = true;
                     foreach (var snoMonsterAffix in monster.AffixSnoList)
                     {
                         string affixName = null;
@@ -236,7 +233,7 @@ namespace Turbo.Plugins.Default
                             affixName = CustomAffixNames[snoMonsterAffix.Affix];
                         }
                         else affixName = snoMonsterAffix.NameLocalized;
-                        if (snoMonsterAffix.Affix == MonsterAffix.Juggernaut) flag = false;
+                        if (snoMonsterAffix.Affix == MonsterAffix.Juggernaut) flagIsNotJuggernaut = false;
 
                         WorldDecoratorCollection decorator;
                         if (!AffixDecorators.TryGetValue(snoMonsterAffix.Affix, out decorator)) continue;
@@ -244,11 +241,16 @@ namespace Turbo.Plugins.Default
                     }
                     if (monster.Rarity == ActorRarity.Rare)
                     {
-                        if (flag) RareDecorator.Paint(layer, monster, monster.FloorCoordinate, monster.SnoMonster.NameLocalized);
+                        if (flagIsNotJuggernaut) RareDecorator.Paint(layer, monster, monster.FloorCoordinate, monster.SnoMonster.NameLocalized);
                         else JuggernautDecorator.Paint(layer, monster, monster.FloorCoordinate, monster.SnoMonster.NameLocalized);
                     }
                     if (monster.Rarity == ActorRarity.Champion) ChampionDecorator.Paint(layer, monster, monster.FloorCoordinate, monster.SnoMonster.NameLocalized);
-
+                    if (currentMonsterHealthPercent < 30.0)
+                    {
+                        string monsterHealthPercentStr = "HP:" + currentMonsterHealthPercent.ToString("0.0") + "%";
+                        EliteHealthDecorator.TextFunc = () => monsterHealthPercentStr;
+                        EliteHealthDecorator.Paint(monsterScreenCoordinate.X, monsterScreenCoordinate.Y + EliteHealthBlockSize / 2, EliteHealthBlockSize, EliteHealthBlockSize, HorizontalAlign.Center);
+                    }
                 }
 
             }
@@ -261,46 +263,39 @@ namespace Turbo.Plugins.Default
         /************************************************************************/
         /* StrickenPlugin                                                       */
         /************************************************************************/
-        public int StrickenRank { get; set; }
-        public int propSquare { get; set; }
-        public bool cooldown { get; set; }
-        public int monsterCount { get; set; }
-        public int offsetX { get; set; }
-        public int offsetY { get; set; }
+        public int StrickenGemLevel { get; set; }
+        public int StrickenPropSquare { get; set; }
+        public bool StrickenInCooldown { get; set; }
+        public int StrickenMonsterCount { get; set; }
         public TopLabelDecorator StrickenStackDecorator { get; set; }
-        public TopLabelDecorator StrickenPercentDecorator { get; set; }
-        public Dictionary<uint, Tuple<double, int>> MonsterStatus { get; set; }  // AcdId, Health, Stacks
+        public Dictionary<uint, Tuple<double, int>> StrickenMonsterStatus { get; set; }  // AcdId, Health, Stacks
         public void InitStrickenPlugin()
         {
-            offsetX = 0;
-            offsetY = 0;
-            MonsterStatus = new Dictionary<uint, Tuple<double, int>>();
+            StrickenMonsterStatus = new Dictionary<uint, Tuple<double, int>>();
         }
         public void LoadStrickenPlugin()
         {
-            StrickenRank = 0;
-            propSquare = (int)(Hud.Window.Size.Width / 53.333);
-            cooldown = false;
-            monsterCount = 0;
+            StrickenGemLevel = 0;
+            StrickenPropSquare = (int)(Hud.Window.Size.Width / 53.333);
+            StrickenInCooldown = false;
+            StrickenMonsterCount = 0;
 
             StrickenStackDecorator = new TopLabelDecorator(Hud)
             {
                 TextFont = Hud.Render.CreateFont("tahoma", 7, 255, 0, 0, 0, true, false, 250, 255, 255, 255, true),
             };
 
-            StrickenPercentDecorator = new TopLabelDecorator(Hud)
-            {
-                //TextFont = Hud.Render.CreateFont("tahoma", 6, 255, 255, 255, 255, false, false, 250, 0, 0, 0, true),
-                TextFont = Hud.Render.CreateFont("tahoma", 6, 255, 255, 36, 0, false, false, 250, 0, 0, 0, true),
-            };
-
+            //      StrickenPercentDecorator = new TopLabelDecorator(Hud)
+            //      {
+            //          TextFont = Hud.Render.CreateFont("tahoma", 6, 255, 255, 255, 255, false, false, 250, 0, 0, 0, true),
+            //      };
 
         }
         public void OnNewAreaStrickenPlugin(bool newGame, ISnoArea area)
         {
             if (newGame)
             {
-                MonsterStatus.Clear();
+                StrickenMonsterStatus.Clear();
             }
         }
         public void DrawStrickenPlugin(WorldLayer layer)
@@ -313,10 +308,10 @@ namespace Turbo.Plugins.Default
                 if (StrickenLocation.SocketCount != 1 || StrickenLocation.ItemsInSocket == null) continue;
                 var Stricken = StrickenLocation.ItemsInSocket.FirstOrDefault();
                 if (Stricken == null) continue;
-                if (Stricken.SnoItem.Sno == 3249948847) { StrickenActive = true; StrickenRank = Stricken.JewelRank; break; } else { continue; }
+                if (Stricken.SnoItem.Sno == 3249948847) { StrickenActive = true; StrickenGemLevel = Stricken.JewelRank; break; } else { continue; }
             }
             if (StrickenActive == false) return;
-            float gemMaths = 0.8f + (0.01f * (float)StrickenRank);
+            float gemMaths = 0.8f + (0.01f * (float)StrickenGemLevel);
             var Texture = Hud.Texture.GetItemTexture(Hud.Sno.SnoItems.Unique_Gem_018_x1);
             var monsters = Hud.Game.Monsters.OrderBy(i => i.NormalizedXyDistanceToMe);
             foreach (var monster in monsters)
@@ -325,68 +320,48 @@ namespace Turbo.Plugins.Default
                 {
                     var monsterScreenCoordinate = monster.FloorCoordinate.ToScreenCoordinate();
                     Tuple<double, int> valuesOut;
-                    if (MonsterStatus.TryGetValue(monster.AcdId, out valuesOut))
+                    if (StrickenMonsterStatus.TryGetValue(monster.AcdId, out valuesOut))
                     {
                         double monHealth = monster.CurHealth;
                         double prevHealth = valuesOut.Item1;
                         int prevStacks = valuesOut.Item2;
 
-                        if (prevHealth > monHealth && Hud.Game.Me.Powers.BuffIsActive(Hud.Sno.SnoPowers.BaneOfTheStrickenPrimary.Sno, 2) && monsterCount == 0 && !cooldown)
+                        if (prevHealth > monHealth && Hud.Game.Me.Powers.BuffIsActive(Hud.Sno.SnoPowers.BaneOfTheStrickenPrimary.Sno, 2) && StrickenMonsterCount == 0 && !StrickenInCooldown)
                         {
                             int Stacks = (int)(prevStacks + 1);
                             Tuple<double, int> updateValues = new Tuple<double, int>(monster.CurHealth, Stacks);
-                            MonsterStatus[monster.AcdId] = updateValues;
-                            monsterCount++;
-                            cooldown = true;
+                            StrickenMonsterStatus[monster.AcdId] = updateValues;
+                            StrickenMonsterCount++;
+                            StrickenInCooldown = true;
                         }
 
-                        else if (!Hud.Game.Me.Powers.BuffIsActive(Hud.Sno.SnoPowers.BaneOfTheStrickenPrimary.Sno, 2) && cooldown)
+                        else if (!Hud.Game.Me.Powers.BuffIsActive(Hud.Sno.SnoPowers.BaneOfTheStrickenPrimary.Sno, 2) && StrickenInCooldown)
                         {
-                            cooldown = false;
-                            monsterCount = 0;
+                            StrickenInCooldown = false;
+                            StrickenMonsterCount = 0;
                             if (prevHealth > monHealth)
                             {
                                 int Stacks = (int)(prevStacks);
                                 Tuple<double, int> updateValues = new Tuple<double, int>(monster.CurHealth, Stacks);
-                                MonsterStatus[monster.AcdId] = updateValues;
+                                StrickenMonsterStatus[monster.AcdId] = updateValues;
                             }
                         }
                         if (prevStacks > 0)
                         {
-                            //  int bossPerc = 0;
-                            //  if (monster.SnoMonster.Priority == MonsterPriority.boss) { bossPerc = 25; }
-                            //  else { bossPerc = 0; }
-                            //  float StrickenDamagePercent = (float)(bossPerc + (prevStacks * gemMaths));
-                            //  string percentDamageBonus = "+" + StrickenDamagePercent.ToString("0.00") + "%";
-                            Texture.Draw(monsterScreenCoordinate.X + offsetX, monsterScreenCoordinate.Y + offsetY, propSquare, propSquare);
+                            Texture.Draw(monsterScreenCoordinate.X, monsterScreenCoordinate.Y , StrickenPropSquare, StrickenPropSquare);
                             StrickenStackDecorator.TextFunc = () => prevStacks.ToString();
-                            //  StrickenPercentDecorator.TextFunc = () => percentDamageBonus;
-                            StrickenStackDecorator.Paint(monsterScreenCoordinate.X + offsetX, monsterScreenCoordinate.Y + offsetY, propSquare, propSquare, HorizontalAlign.Center);
-                            //  StrickenPercentDecorator.Paint(monsterScreenCoordinate.X + offsetX, (float)(monsterScreenCoordinate.Y + offsetY + (propSquare / 2.5)), propSquare, propSquare, HorizontalAlign.Right);
-
-                            var currentMonsterHealthPercent = monster.CurHealth * 100.0 / monster.MaxHealth;
-                            if (currentMonsterHealthPercent < 30.0)
-                            {
-                                string monsterHealthPercentStr = "HP:" + currentMonsterHealthPercent.ToString("0.00") + "%";
-                                StrickenPercentDecorator.TextFunc = () => monsterHealthPercentStr;
-                                StrickenPercentDecorator.Paint(monsterScreenCoordinate.X + offsetX, (float)(monsterScreenCoordinate.Y + offsetY + (propSquare / 2.5)), propSquare, propSquare, HorizontalAlign.Right);
-                            }
-                            //      if (cooldown)
-                            //      {
-                            //          StrickenPercentDecorator.TextFunc = () => "\u231B";
-                            //          StrickenPercentDecorator.Paint((float)(monsterScreenCoordinate.X + offsetX + (propSquare / 2)), monsterScreenCoordinate.Y + offsetY, propSquare, propSquare, HorizontalAlign.Center);
-                            //      }
+                            StrickenStackDecorator.Paint(monsterScreenCoordinate.X, monsterScreenCoordinate.Y , StrickenPropSquare, StrickenPropSquare, HorizontalAlign.Center);
                         }
                     }
                     else
                     {
                         Tuple<double, int> valuesIn = new Tuple<double, int>(monster.CurHealth, (int)(0));
-                        MonsterStatus.Add(monster.AcdId, valuesIn);
+                        StrickenMonsterStatus.Add(monster.AcdId, valuesIn);
                     }
                 }
                 else
                 {
-                    MonsterStatus.Remove(monster.AcdId);
+                    StrickenMonsterStatus.Remove(monster.AcdId);
                 }
             }
         }
